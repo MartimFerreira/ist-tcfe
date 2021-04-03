b4701787a4c1deca87ff4ed9f722fd9292d2dc23
MartimFerreira <martim.marques.ferreira@gmail.com>
2021-04-03 20:53:58 +0100

    Minor tweeks to octave and changes to graphs in t2_4.net

--- t2/mat/rc.m
+++ t2/mat/rc.m
@@ -5,86 +5,6 @@ clear all
 
 format long
 
-%{
-pkg load symbolic
-
-syms t
-syms R
-syms C
-syms vi(t)
-syms vo(t)
-syms i(t)
-
-i(t)=C*diff(vo,t)
-
-printf("\n\nKVL equation:\n");
-
-vi(t) = R*i(t)+vo(t)
-
-syms vo_n(t) %natural solution
-syms vo_f(t) %forced solution
-
-printf("\n\nSolution is of the form");
-
-v(t) = vo_n(t) + vo_f(t)
-
-printf("\n\nNatural solution:\n");
-syms A
-syms wn
-
-vi(t) = 0 %no excitation
-i_n(t) = C*diff(vo_n, t)
-
-
-printf("\n\n Natural solution is of the form");
-vo_n(t) = A*exp(wn*t)
-
-R*i_n(t)+vo_n(t) == 0
-
-R*C*wn*vo_n(t)+vo_n(t) == 0
-
-R*C*wn+1==0
-
-solve(ans, wn)
-
-
-%%EXAMPLE NUMERIC COMPUTATIONS
-
-R=1e3 %Ohm
-C=100e-9 %F
-
-f = 1000 %Hz
-w = 2*pi*f; %rad/s
-
-%time axis: 0 to 10ms with 1us steps
-t=0:1e-6:10e-3; %s
-
-Zc = 1/(j*w*C)
-Cgain = Zc/(R+Zc)
-Gain = abs(Cgain)
-Phase = angle(Cgain)
-
-vi = 1*cos(w*t);
-vo = Gain*cos(w*t+Phase);
-
-hf = figure ();
-plot (t*1000, vi, "g");
-hold on;
-plot (t*1000, vo, "b");
-
-xlabel ("t[ms]");
-ylabel ("vi(t), vo(t) [V]");
-print (hf, "forced.eps", "-depsc");
-
-
-%}
-
-
-%%%%%%% TEMPORARY %%%%%%%%
-
-
-
-
 %%%%%%%%%%%%%%%CREATE-NGSPICE-INPUT-DATA-FILE%%%%%%%%%%%%%%%%%%
 
 data = fopen("../data.txt", "r");
@@ -109,6 +29,8 @@ trash = fscanf(data, "\n\nUnits for the values: V, mA, kOhm, mS and uF\n\nValues
 fclose(data)
 
 
+%%%%%%%%%%%%%%%%%%%%%%% MAIN CALCULATIONS %%%%%%%%%%%%%%%%555
+
 fR1 = fR1 * 1000;
 fR2 = fR2 * 1000;
 fR3 = fR3 * 1000;
@@ -120,6 +42,8 @@ fC = fC/1000000;
 fKb = fKb/1000;
 fKd = fKd*1000;
 
+
+%%%%%%%%% t < 0
 M = [ 1, 0, 0, -1, 0, 0, 0, 0;
       -(1/fR1), (1/fR1) + (1/fR2) + (1/fR3), -(1/fR2),0,  -(1/fR3), 0, 0, 0;
       0, -(1/fR2) - fKb, (1/fR2), 0, fKb, 0, 0, 0;
@@ -143,6 +67,9 @@ V8 = x(8)
 
 Vx= V8-V6; 
 
+
+
+%%%%%% t = 0
 M0 = [(1/fR1) + (1/fR2) + (1/fR3), -(1/fR2), -(1/fR3), 0, 0, 0, 0;
       1/fR2 - fKb, 1/fR2, fKb, 0, 0, 0, 0;
       fKb, 0, -fKb - 1/fR5, 1/fR5, 0, 0, 1;
