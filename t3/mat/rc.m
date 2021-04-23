5e51ee77ee5680da32aa9d1e0677803523f6535c
Inês Moreira <ines.p.moreira@tecnico.ulisboa.pt>
2021-04-23 11:24:31 +0100

    rc.m corrected and makefiles sim and mat changed. Makefile of sim still making errors

--- t3/mat/rc.m
+++ t3/mat/rc.m
@@ -58,7 +58,7 @@ fprintf(ngspice_input,"Dreg3 d2d3 0 Default\n");
 fprintf(ngspice_input,"L1 begin1 begin2 %.12f\n", fL1);
 fprintf(ngspice_input,"L2 in1 in2 %.12f\n", fL2);
 fprintf(ngspice_input,"K L1 L2 1 Default\n");
-% if 1 doesn't work maybe try 0.99999
+% if 1 doesnt work maybe try 0.99999
 
 
 %%% voltage source
@@ -69,36 +69,36 @@ fprintf(ngspice_1_input,"Vs begin1 begin2 230 AC %.12f SIN(0.0 1.0 50.0) %.12f\n
 %%%%%%%%%%%%%%%%%%%%%%%% TABLES %%%%%%%%%%%%%%%%%%%%%%
 
 
-data_tab = fopen("octave_data_tab.tex", "w");
+%data_tab = fopen("octave_data_tab.tex", "w");
 
-fprintf(data_tab, "$R_1\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR1);
-fprintf(data_tab, "$R_2\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR2);
-fprintf(data_tab, "$R_3\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR3);
-fprintf(data_tab, "$R_4\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR4);
-fprintf(data_tab, "$R_5\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR5);
-fprintf(data_tab, "$R_6\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR6);
-fprintf(data_tab, "$R_7\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR7);
-fprintf(data_tab, "$V_s\\;(V)$ & %.12e \\\\ \\hline\n", fVs);
-fprintf(data_tab, "$C\\;(F)$ &   %.12e \\\\ \\hline\n", fC);
-fprintf(data_tab, "$K_b\\;(S)$ & %.12e \\\\ \\hline\n", fKb);
-fprintf(data_tab, "$K_d\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fKd);
+%fprintf(data_tab, "$R_1\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR1);
+%fprintf(data_tab, "$R_2\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR2);
+%fprintf(data_tab, "$R_3\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR3);
+%fprintf(data_tab, "$R_4\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR4);
+%fprintf(data_tab, "$R_5\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR5);
+%fprintf(data_tab, "$R_6\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR6);
+%fprintf(data_tab, "$R_7\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fR7);
+%fprintf(data_tab, "$V_s\\;(V)$ & %.12e \\\\ \\hline\n", fVs);
+%fprintf(data_tab, "$C\\;(F)$ &   %.12e \\\\ \\hline\n", fC);
+%fprintf(data_tab, "$K_b\\;(S)$ & %.12e \\\\ \\hline\n", fKb);
+%fprintf(data_tab, "$K_d\\;(\\Omega)$ & %.12e \\\\ \\hline\n", fKd);
 
-fclose(data_tab);
+%fclose(data_tab);
 
 
-zero_time_tab = fopen("octave_zero_time_tab.tex", "w");
+%zero_time_tab = fopen("octave_zero_time_tab.tex", "w");
 
-fprintf(zero_time_tab, "$V_1$ & %.12e \\\\ \\hline\n", 0);
-fprintf(zero_time_tab, "$V_2$ & %.12e \\\\ \\hline\n", V20);
-fprintf(zero_time_tab, "$V_3$ & %.12e \\\\ \\hline\n", V30);
-fprintf(zero_time_tab, "$V_5$ & %.12e \\\\ \\hline\n", V50);
-fprintf(zero_time_tab, "$V_6$ & %.12e \\\\ \\hline\n", V60);
-fprintf(zero_time_tab, "$V_7$ & %.12e \\\\ \\hline\n", V70);
-fprintf(zero_time_tab, "$V_8$ & %.12e \\\\ \\hline\n", V80);
-fprintf(zero_time_tab, "$V_x$ & %.12e \\\\ \\hline\n", Vx);
-fprintf(zero_time_tab, "$I_x$ & %.12e\\\\ \\hline\n", Ix);
-fprintf(zero_time_tab, "$R_{eq}$ & %.12e \\\\ \\hline\n", Req);
+%fprintf(zero_time_tab, "$V_1$ & %.12e \\\\ \\hline\n", 0);
+%fprintf(zero_time_tab, "$V_2$ & %.12e \\\\ \\hline\n", V20);
+%fprintf(zero_time_tab, "$V_3$ & %.12e \\\\ \\hline\n", V30);
+%fprintf(zero_time_tab, "$V_5$ & %.12e \\\\ \\hline\n", V50);
+%fprintf(zero_time_tab, "$V_6$ & %.12e \\\\ \\hline\n", V60);
+%fprintf(zero_time_tab, "$V_7$ & %.12e \\\\ \\hline\n", V70);
+%fprintf(zero_time_tab, "$V_8$ & %.12e \\\\ \\hline\n", V80);
+%fprintf(zero_time_tab, "$V_x$ & %.12e \\\\ \\hline\n", Vx);
+%fprintf(zero_time_tab, "$I_x$ & %.12e\\\\ \\hline\n", Ix);
+%fprintf(zero_time_tab, "$R_{eq}$ & %.12e \\\\ \\hline\n", Req);
 
-fclose(zero_time_tab);
+%fclose(zero_time_tab);
 
 
