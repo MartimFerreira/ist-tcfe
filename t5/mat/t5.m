bc1fccccf55e1647313d8855593cfbc78f4f7f73
Inês Moreira <ines.p.moreira@tecnico.ulisboa.pt>
2021-06-05 17:47:34 +0100

    several changes t5.m

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,13 +1,65 @@
-pkg load control
+C1=220e-9;
+C2=220e-9;
+R1=1e3;
+R2=1e3;
+R3=100e3;
+R4=1e3;
 
-%Componentes
-R1=1000
-R2=10000
+f=1000;
 
+w=2*pi*f;
+
+Zc1=1/(j*w*C1);
+Zc2=1/(j*w*C2);
+
+Zin=abs(Zc1+R1)
+Zout=abs(1/(1/R2+1/Zc2))
+
+vp=R1/(R1+Zc1);
+amp=(1+R3/R4)*vp;
+gain=abs(Zc2/(Zc2+R2)*amp)
+gain_db=20*log10(gain)
+
+
+
+
+
+t=1:0.1:8;
+
+
+
+w=2*pi*power(10,t);
+
+Zc1=1./(j*w*C1);
+Zc2=1./(j*w*C2);
+
+vp=R1./(R1+Zc1);
+amp=(1+R3/R4).*vp;
+fgain=abs(Zc2./(Zc2+R2).*amp);
+fgain_db=20*log10(fgain);
+
+[max_gain,index_freq_central]=max(fgain_db)
+
+
+
+
+
+
+freq_graph = figure();
+plot (t, fgain_db, "m");
+legend("Gain");
+xlabel ("log_{10}(f) [Hz]");
+ylabel ("dB");
+print (freq_graph, "graph.eps", "-depsc");
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  
 % para o circuito que o professor pôs inicialmente no ngspice
-Gain = -R2/R1;
-Zi = R1;
-Zo = 0;
+%Gain = -R2/R1;
+%Zi = R1;
+%Zo = 0;
 % queremos ter um Gain e Zi elevados e uma Zo baixa
 
 
@@ -16,103 +68,8 @@ Zo = 0;
 % Zi = Vi/Ii quando Zl = inf
 % Zo = Vo/Io quando Vi = 0
 
-freq=logspace(1,8, k1);
-
-
 % sites que acho que podem dar jeito
 %https://www.electronics-tutorials.ws/filter/filter_7.html
 %http://www.learningaboutelectronics.com/Articles/Active-op-amp-bandpass-filter-circuit.php
-
-
-
-
-
-
-
-
-
-
- 
-% 8 decades and 10 points per decade => 80 points
-
-
-%k1 = 80
-
-%freq1=logspace(1,8, k1);
-
-
-
-%t1=1
-%w1=2*pi*freq1
-VS1=0.01							       
-	   
-%gain1 = - gm1 * (RC1*ro1)/(RC1+ro1) * ((RB1*RB2+rpi1*RB2+rpi1*RB1)/(rpi1*RB1*RB2)) / (RS+(RB1*RB2+rpi1*RB2+rpi1*RB1)/(rpi1*RB1*RB2)) % * VS1
-
-
-%%%%%%%%%%%%%%%   MAG TO DB   %%%%%%%%%%%%%%%%%%%%%%%%
-%AV1wdb = mag2db(abs(AV1w))
-%AV1simplewdb = mag2db(abs(AV1simplew))
-  
-%AV1db = mag2db(abs(AV1))
-%gain1db = mag2db(abs(gain1))
-%AV1simpledb= mag2db(abs(AV1simple))
-%AV1simple2db= mag2db(abs(AV1simple2))
-  
-
-%vmag_plot1 = figure ();
-%semilogx (freq1, abs(AV1simple2db), "r");
-
-%xlabel ("f[Hz]");
-%ylabel ("Vo(f)/Vi(f)[dB]"); 
-%legenda= legend("Frequency response"); 
-%print (vmag_plot1, "vmag_plot1.eps", "-depsc");
-
-
-
-
-
-%AV22= mag2db(abs(AV2))
-
-%vmag_plot2 = figure ();
-%semilogx (freq1, AV2, "b");
-
-%xlabel ("f[Hz]");
-%ylabel ("Vo(f)/Vi(f)[dB]"); 
-%legenda= legend("Frequency response"); 
-%print (vmag_plot2, "vmag_plot2.eps", "-depsc");
-
-
-%final_gain= AV1simple2*AV2
-%final_gaindb= mag2db(abs(final_gain))
-
-%final_impedance = ZO1*ZO2  
-	      
-		     
-%vmag_plot3 = figure ();
-%semilogx (freq1, final_gaindb, "r");
-
-%xlabel ("f[Hz]");
-%ylabel ("Vo(f)/Vi(f)[dB]"); 
-%legenda= legend("Frequency response"); 
-%print (vmag_plot3, "vmag_plot3.eps", "-depsc");
-
-
-
-
-%%%%%%%%%%%%%% TABLE WITH VALUES (FOR LATEX) %%%%%%%%%%%%%%%%%%%%
-%resistance_tab = fopen("resistance_tab.tex", "w");
-
-%fprintf(resistance_tab, "$V_{CC}\\;(V)$ & %.1e\\\\ \\hline\n", VCC);
-%fprintf(resistance_tab, "$R_{S}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RS);
-%fprintf(resistance_tab, "$R_{E1}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RE1);
-%fprintf(resistance_tab, "$R_{C1}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RC1);
-%fprintf(resistance_tab, "$R_{B1}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RB1);
-%fprintf(resistance_tab, "$R_{B2}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RB2);
-%fprintf(resistance_tab, "$R_{E2}\\;(\\Omega)$ & %.1e\\\\ \\hline\n", RE2);
-%fprintf(resistance_tab, "$C_{i}\\;(mF)$ & %.1e\\\\ \\hline\n", 1);
-%fprintf(resistance_tab, "$C_{e}\\;(mF)$ & %.1e\\\\ \\hline\n", 0.5);
-%fprintf(resistance_tab, "$C_{o}\\;(mF)$ & %.1e\\\\ \\hline\n", 0.5);
-
-%fclose(resistance_tab);
 
 
