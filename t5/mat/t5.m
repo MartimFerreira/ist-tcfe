9dab6ed451840916d7827057af5a6212b863aecf
Inês Moreira <ines.p.moreira@tecnico.ulisboa.pt>
2021-06-08 19:55:47 +0100

    octave

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,147 +1,92 @@
-
-
 %Componentes
+
 
 R1=10e3;
 R2=5e2;
+
 R3=100e3;
 R4=1e3;
-R5=100e3; 
-C1=220e-9;
-C2=220e-9;
-C3=220e-9; 
 
+
+R2tab=1e3;
+R5tab=1e3;
+
+R8=10e3;
+
+C1=220e-9;
+C2=110e-9;
+
+C2tab=220e-9;
+C3tab=220e-9;
+
+
+
+R1
+R1eq=(R1*R8/(R1+R8))
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Exercicio 1 
+
 
 f=1000;
 
 w=2*pi*f;
 
-%Impedancias condensadores
 ZC1=1/(j*w*C1);
-ZC2=1/(j*w*C2);
-ZC3=1/(j*w*C3);
-Zpar=(1/(1/R5+1/ZC3));
-
-
-%Impedancia in & out 
-Zin=abs(ZC1+R1)
-  Zout=abs(1/(1/R2+1/(ZC2+Zpar)));
-
-  
-%high  pass filter
-
-Vi=1; 
-Vl=R1/(R1+ZC1)*Vi;
-wl=1/(R1*C1);       %low cut off frequency
+ZC2=1/(j*w*C2)
 
 
 
-%Amplification
-Va=(1+R3/R4)*Vl;
+fl=1/(R1eq*C1*2*pi)
+fh=1/(R2*C2*2*pi)
+fo=sqrt(fh*fl)
 
 
 
-%low pass filter
-Vh= Va;
-wh=1/(R2*(C2/2));      %high cut off frequency
 
 
-gain=abs(Vh);
+Zi=abs(ZC1+R1eq)
+Zo=abs(1/(1/R2+1/ZC2))
+
+Vl=R1eq/(R1eq+ZC1)
+A=(1+R3/R4)*Vl
+hpass=abs(ZC2/(ZC2+R2))
+gain=abs(ZC2/(ZC2+R2)*A)
 gain_db=20*log10(gain)
 
-gain_dev=gain_db-40
+
+gain_dev=gain_db-40;
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Exercicio 2
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Exercicio2
 t=1:0.1:8;
+
+
 
 w=2*pi*power(10,t);
 
 ZC1=1./(j*w*C1);
-ZC2=1./(j*w*(C2+C3)); 
-ZC3=1./(j*w*C3);
+ZC2=1./(j*w*C2);
 
-Vl=R1./(R1+ZC1);
+
+
+Vl=R1eq./(R1eq+ZC1);
 A=(1+R3/R4).*Vl;
 fgain=abs(ZC2./(ZC2+R2).*A);
-
-
 fgain_db=20*log10(fgain);
 
 max_gain=max(fgain_db)
 
+aux=1
 
-  
-freq_graph = figure();
+
+
+freq = figure();
 plot (t, fgain_db, "m");
 legend("Gain");
 xlabel ("log_{10}(f) [Hz]");
 ylabel ("dB");
-print (freq_graph, "graph.eps", "-depsc");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%modelo nao ideal
-
-%modelo nao ideal ampop
-%Zi=966.3157e3 + j*2.850914e-2
-%Zo=5.000960e1 - j*5.09420e-3
-%Av=(Zo+1)*(-1.12308e-2 -j*1.95016e-1)
-
-
-%freq=logspace(1,8,7000);
-
-%for i=freq
-
-
-  
-%w=2*pi*i;
-
-
-%ZC1=1./(j*w*C1);
-%ZC2=1./(j*w*C2);
-%ZC3=1./(j*w*C3);
-
-%Vl=R1./(R1+ZC1);
-%A=(1+R3/R4).*Vl;
-%Vh= A;
-%fgain=abs(Vh);
-%fgain_db=20*log10(fgain);
-
-%max_gain=max(fgain_db);
-
-%Zeq= ZC2+(ZC3*R5/(ZC3+R5));
-
-%B=[1/Zo + 1/(Zeq) + 1/R3 , -Av/Zo + 1/R3;
-%      1/R3 , 1/Zi + 1/R3 + 1/R4;];
-
-%C = [Vl/R3 , Vl/R3 + Vl/R4];
-
-%D= C/B;
-
-%Vout=D(1)
-
-%endfor
-
-%fgain=abs(Vout/Vi);
-%fgain_db=20*log10(fgain);
+print (freq, "freq.eps", "-depsc");
 
 
 
@@ -153,18 +98,15 @@ print (freq_graph, "graph.eps", "-depsc");
 components_tab = fopen("components_tab.tex", "w");
 
 fprintf(components_tab, "$C_1\\;(F)$ & %.6e\\\\ \\hline\n", C1);
-fprintf(components_tab, "$C_2\\;(F)$ & %.6e\\\\ \\hline\n", C2);
-fprintf(components_tab, "$C_3\\;(F)$ & %.6e\\\\ \\hline\n", C3);
-%fprintf(components_tab, "$C_4\\;(F)$ & %.6e\\\\ \\hline\n", C4);
+fprintf(components_tab, "$C_2\\;(F)$ & %.6e\\\\ \\hline\n", C2tab);
+fprintf(components_tab, "$C_3\\;(F)$ & %.6e\\\\ \\hline\n", C3tab);
 fprintf(components_tab, "$R_1\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R1);
-fprintf(components_tab, "$R_2\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R2);
+fprintf(components_tab, "$R_2\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R2tab);
 fprintf(components_tab, "$R_3\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R3);
 fprintf(components_tab, "$R_4\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R4);
-fprintf(components_tab, "$R_5\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R5);
-%fprintf(components_tab, "$R_6\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R6);
-%fprintf(components_tab, "$R_7\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R7);
-%fprintf(components_tab, "$R_8\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R8);
-%fprintf(components_tab, "$R_9\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R9);
+fprintf(components_tab, "$R_5\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R5tab);
+fprintf(components_tab, "$R_6\\;(\\Omega)$ & %.6e\\\\ \\hline\n", R8);
+
 
 fclose(components_tab);
 
@@ -172,10 +114,12 @@ fclose(components_tab);
 
 results_tab = fopen("results_tab.tex", "w");
 
-fprintf(results_tab, "$Z_{in}\\;(\\Omega)$ & %.6e\\\\ \\hline\n", Zin);
-fprintf(results_tab, "$Z_{out}\\;(\\Omega)$ & %.6e\\\\ \\hline\n", Zout);
-fprintf(results_tab, "$Gain$ & %.6e\\\\ \\hline\n", fgain);  % pôr também em dB???
-fprintf(results_tab, "$f_{low \ cut \ off}\\;(Hz)$ & %.6e\\\\ \\hline\n", wl);
-fprintf(results_tab, "$f_{high \ cut \ off}\\;(Hz)$ & %.6e\\\\ \\hline\n", wh);
+fprintf(results_tab, "$Z_{in}\\;(\\Omega)$ & %.6e\\\\ \\hline\n", Zi);
+fprintf(results_tab, "$Z_{out}\\;(\\Omega)$ & %.6e\\\\ \\hline\n", Zo);
+fprintf(results_tab, "$Gain$ & %.6e\\\\ \\hline\n", fgain);
+fprintf(results_tab, "$f_{low \ cut \ off}\\;(Hz)$ & %.6e\\\\ \\hline\n", fl);
+fprintf(results_tab, "$f_{high\ cut \ off}\\;(Hz)$ & %.6e\\\\ \\hline\n", fh);
+fprintf(results_tab, "$f_{central}\\;(Hz)$ & %.6e\\\\ \\hline\n", fo);
+
 
 fclose(results_tab);
